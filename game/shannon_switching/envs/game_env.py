5b85ebc826b4dffcc5f67c2b0ae64950d3fcd6f3
divyansh <somm14divi@gmail.com>
2018-11-15 02:08:23 +0530

    Working environment

--- game/shannon_switching/envs/game_env.py
+++ game/shannon_switching/envs/game_env.py
@@ -1,75 +1,66 @@
-# import gym
-# from gym import error, spaces, utils
-# from gym.utils import seeding
-from ShanonnGraph import ShannonGraph
-import Queue
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+from ShannonGraph import ShannonGraph
+
 graph_file = 'graph.txt'
 
 
-def initialize_game_state(graph):
-	N = len(graph[0])
-	state = [[1 0 0 0] for i in range(N*N)]
-	for i in range(N):
-		for j in range(i+1,N):
-			if graph[i][j]:
-				state[i*N+j] = [0 1 0 0]
-	return state
-
-GameGraph = ShannonGraph('graph',0)
-
-#state is a configuration of the graph 
 class Env(gym.Env):
-	metadata = {'render.modes': ['human']}
+    metadata = {'render.modes': ['human']}
 
-	def __init__(self):
-		self.numEdges = len(GameGraph.getEdges())
-		self.edges = GameGraph.getEdges()
-		self.N = GameGraph.N
-		self.action_space = spaces.Discrete(numEdges)
-		self.observation_space = spaces.MultiDiscrete([3 for i in range(numEdges)])
-		self.ishumanFirstPlayer = GameGraph.ishumanFirstPlayer
-		#[self.edge_map, self.reverse_edge_map] = GameGraph.getEdgeMap()
-		self.seed()
-		self.reset()
-	#action is a number 
-	def step(self, action):
-		[v1,v2] = self.edges[action]	
-		humanMove = [min(v1,v2),max(v1,v2)]
-		if not GameGraph.isPlayableEdge(humanMove):
-			return [self.observation,0,0,None]
-		GameGraph.playHumanMove(humanMove)
-		idx = self.edges.index([computerMove[0],computerMove[1]])
-		self.observation[idx]=1 #humanPlay
-		over = GameGraph.isgameover()
-		if over!=0:
-			return [self.observation,over,1,None]
-		humanMove = [v1,v2,v1*self.N+v2]
-		computerMove = GameGraph.getComputerMove(humanMove)
-		if computerMove[0]!=-1:
-			GameGraph.playComputerMove(computerMove)
-			idx = self.edges.index([computerMove[0],computerMove[1]])
-			self.observation[idx]=2
+    def __init__(self):
+        self.ishumanFirstPlayer = 0
+        self.ishumanCut = 1
+        self.gameGraph = ShannonGraph('graph', self.ishumanCut, self.ishumanFirstPlayer)
+        self.numActions = len(self.gameGraph.getEdges())
+        self.N = self.gameGraph.N
+        self.action_space = spaces.Discrete(self.numActions)
+        self.observation_space = spaces.MultiDiscrete([3 for __ in range(self.numActions)])
+        self.actionEdgeMap = {}
+        self.edgeActionMap = {}
+        for i, edge in enumerate(self.gameGraph.edges):
+            self.actionEdgeMap[i] = [min(edge[0], edge[1]), max(edge[0], edge[1])]
+            self.edgeActionMap[(min(edge[0], edge[1]), max(edge[0], edge[1]))] = i
+        self.seed()
+        self.reset()
 
-		over = GameGraph.isgameover()
-		if over==0:
-			return [self.observation,0,0,None]
-		else:
-			return [self.observation,over,1,None]	
+    def step(self, action):
+        humanMove = self.actionEdgeMap[action]
+        if not self.gameGraph.isPlayableEdge(humanMove):
+            return [self.observation, 0, 0, None]
+        self.gameGraph.playHumanMove(humanMove)
+        idx = self.edgeActionMap[(humanMove[0], humanMove[1])]
+        self.observation[idx] = 1
+        over = self.gameGraph.isgameover()
+        if over == 1:
+            return [self.observation, over, 1, None]
+        humanMove = [humanMove[0], humanMove[1], humanMove[0] * self.N + humanMove[1]]
+        computerMove = self.gameGraph.getComputerMove(humanMove)
+        print(computerMove)
+        if computerMove[0] != -1:
+            self.gameGraph.playComputerMove(computerMove)
+            idx = self.edgeActionMap[(computerMove[0], computerMove[1])]
+            self.observation[idx] = 2
+        over = self.gameGraph.isgameover()
+        if over == 0:
+            return [self.observation, 0, 0, None]
+        else:
+            return [self.observation, over, 1, None]
 
+    def reset(self):
+        self.observation = [0 for __ in range(self.numActions)]
+        self.gameGraph.reset()
+        if not self.ishumanFirstPlayer:
+            computerMove = self.gameGraph.getComputerMove([0, self.N - 1, self.N * self.N])
+            if computerMove[0] != -1:
+                self.gameGraph.playComputerMove(computerMove)
+                idx = self.edgeActionMap[(computerMove[0], computerMove[1])]
+                self.observation[idx] = 2
 
-	def reset(self):
-		self.observation = [0 for i in range(self.numEdges)] #0 means edge is not played, 1 means edge is played by human, 2 means edge played by computer
-		if not self.ishumanFirstPlayer:
-			computerMove = GameGraph.getComputerMove([0,self.N-1,self.N*self.N])
-			if computerMove[0]!=-1:
-				GameGraph.playComputerMove(computerMove)
-				idx = self.edges.index([computerMove[0],computerMove[1]])
-				self.observation[idx]=2
-		GameGraph.reset()
+    def render(self, mode='human', close=False):
+        raise NotImplementedError
 
-	def render(self, mode='human', close=False):
-		raise NotImplementedError
-
-	def seed(self, seed=None):
-		self.np_random, seed = seeding.np_random(seed)
-		return [seed]	
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
